3858d471803770551374c3c7ebe87c3ab7a09824
Anton Ukhanev <xedin.unknown@gmail.com>
2016-12-02 03:25:55 +0100

    Added WP CLI test bootstrap code (#1)

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,3 +3,14 @@
 error_reporting(E_ALL | E_STRICT);
 
 require_once dirname(__FILE__).'/../vendor/autoload.php';
+
+if (!defined('WP_CLI_ROOT')) {
+    define('WP_CLI_ROOT', dirname(__FILE__).'/../vendor/wp-cli/wp-cli');
+}
+
+include WP_CLI_ROOT.'/php/utils.php';
+include WP_CLI_ROOT.'/php/dispatcher.php';
+include WP_CLI_ROOT.'/php/class-wp-cli.php';
+include WP_CLI_ROOT.'/php/class-wp-cli-command.php';
+
+\WP_CLI\Utils\load_dependencies();
