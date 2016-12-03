090b4b558b5b160377dedfb8c08d3df84eea46ea
Anton Ukhanev <xedin.unknown@gmail.com>
2016-12-03 16:16:51 +0100

    Added generic methods for running commands (#4)
    These help standardize command results.

--- src/Wp/CommandBase.php
+++ src/Wp/CommandBase.php
@@ -3,6 +3,8 @@
 namespace Dhii\WpProvision\Wp;
 
 use RuntimeException;
+use Dhii\WpProvision\Api;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 /**
  * A base for all commands that expose a common command interface.
@@ -161,5 +163,68 @@ abstract class CommandBase extends AbstractCommand implements CommandInterface
         ];
 
         return $info;
+    }
+
+    /**
+     * Runs a command, abstracting details.
+     *
+     * @since [*next-version*]
+     *
+     * @param callable $cmd A callable that runs a CLI command.
+     *
+     * @return Api\CommandResultInterface The command result.
+     */
+    protected function _runCommand($cmd)
+    {
+        if (!is_callable($cmd)) {
+            throw new RuntimeException(sprintf('Could not run command: command must be callable'));
+        }
+
+        $status  = Api\CommandResultInterface::MSG_STATUS_INFO;
+        $message = null;
+        $result  = null;
+        ob_start();
+        try {
+            $result = call_user_func_array($cmd, []);
+        } catch (ProcessFailedException $e) {
+            $status = Api\CommandResultInterface::MSG_STATUS_ERROR;
+        }
+
+        $output = ob_end_clean();
+        $data   = $result;
+
+        if ($result instanceof Api\CommandResultInterface) {
+            if (!is_null($cmdOut = $result->getText())) {
+                $output = $cmdOut;
+            }
+            if (!is_null($cmdMessage = $result->getMessage())) {
+                $message = $cmdMessage;
+            }
+            if (!is_null($cmdStatus = $result->getStatus())) {
+                $status = $cmdStatus;
+            }
+            $data = $result->getData();
+        }
+
+        $result = $this->_createCommandResult($status, $message, $data, $output);
+
+        return $result;
+    }
+
+    /**
+     * Creates a new instance of an object that represents the result of a command.
+     *
+     * @since [*next-version*]
+     *
+     * @param string  $status  Status of the command. One of the {@see CommandResultInterface}::MSG_STATUS_* constants.
+     * @param string  $message Status message.
+     * @param mixed[] $data    Data passed from the command. Usually the result of parsing.
+     * @param string  $text    Full output of the command.
+     *
+     * @return \Dhii\WpProvision\Api\CommandResult
+     */
+    protected function _createCommandResult($status, $message = null, $data = [], $text = null)
+    {
+        return new Api\CommandResult($status, $message, $data, $text);
     }
 }
