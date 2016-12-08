2c9a01fa80aac6f1fbaccdfde60293c4645ad4b0
Anton Ukhanev <xedin.unknown@gmail.com>
2016-12-08 21:50:10 +0100

    Parsing of command output now falls back to status message parsing (#6)

--- src/Output/AbstractOutputBase.php
+++ src/Output/AbstractOutputBase.php
@@ -17,7 +17,7 @@ abstract class AbstractOutputBase extends AbstractOutput implements OutputInterf
     public function __construct($text)
     {
         $this->_setText($text);
-        $info = $this->_parse($text);
+        $info = $this->_getParsedStructure($text);
         $this->_setBody($info[self::K_BODY]);
         $this->_setHeader($info[self::K_HEADER]);
         $this->_setFooter($info[self::K_FOOTER]);
@@ -98,5 +98,49 @@ abstract class AbstractOutputBase extends AbstractOutput implements OutputInterf
     protected function _createStatusMessage($message)
     {
         return new StatusMessage($message);
+    }
+
+    /**
+     * Parses output into a structure.
+     *
+     * Falls back onto parsing a status message if the first attempt throws.
+     * If status message parser throws as well, re-throws the first exception,
+     * preserving the cause.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $output
+     *
+     * @throws ParsingException If the output cannot be parsed.
+     *
+     * @return mixed[] An array that will contain the following indices:
+     *                 K_HEADER, K_BODY, K_FOOTER, K_DATA
+     */
+    protected function _getParsedStructure($output)
+    {
+        try {
+            return $this->_parse($output);
+        } catch (ParsingException $e1) {
+            try {
+                $output = $this->_createFallbackParser($output);
+            } catch (\Exception $e2) {
+                // Throw first exception with added link to new exception
+                throw new ParsingException($e1->getMessage(), $e1->getCode(), $e2);
+            }
+
+            $info = [
+                self::K_HEADER => $output->getMessage(),
+                self::K_BODY   => $output->getText(),
+                self::K_FOOTER => null,
+                self::K_DATA   => $output,
+            ];
+
+            return $info;
+        }
+    }
+
+    protected function _createFallbackParser($output)
+    {
+        return new StatusMessage($output);
     }
 }
